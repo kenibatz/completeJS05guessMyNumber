6653fc7a679ee94618b407b4a540d56be8b5da19
kenibatz <kenny.batallones@gmail.com>
2022-07-05 11:24:28 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*
+create a random secret number
+the user enters a number in the input field and presses check
+if the number is higher or lower than the secret number a point is subtracted from score
+*/
+document.querySelector('.check').addEventListener('click', function () {
+  console.log('you clicked!');
+});
