95e99b94d5f11a751c6b1c1c6e0189d3dfc23f36
kenibatz <kenny.batallones@gmail.com>
2022-07-06 09:47:52 +0800

    version 0.1
    exercise completed with initial refactoring

--- script.js
+++ script.js
@@ -4,6 +4,59 @@ create a random secret number
 the user enters a number in the input field and presses check
 if the number is higher or lower than the secret number a point is subtracted from score
 */
+
+let score = 20;
+let highscore = 0;
+let secretNumber;
+let checkDisabled = false;
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('you clicked Again');
+  score = 20;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.guess').value = 0;
+  document.querySelector('.score').textContent = score;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+  document.querySelector('.check').disabled = false;
+  checkDisabled = false;
+});
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log('you clicked!');
+  if (score > 1) {
+    console.log('you clicked Check');
+    // if (checkDisabled) {
+    //   document.querySelector('.message').textContent = 'press Again.';
+    // } else {
+    const guessNumber = Number(document.querySelector('.guess').value);
+    if (guessNumber) {
+      console.log(`guessNumber: ${guessNumber}`);
+
+      if (guessNumber === secretNumber) {
+        document.querySelector('.message').textContent =
+          'Correct! Press Again to play again.';
+        if (score > highscore) {
+          highscore = score;
+          document.querySelector('.highscore').textContent = highscore;
+        }
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('.check').disabled = true;
+        checkDisabled = true;
+      } else {
+        const hint = guessNumber > secretNumber ? 'Lower' : 'Higher';
+        document.querySelector('.message').textContent = hint;
+        score--;
+        document.querySelector('.score').textContent = score;
+      }
+    } else {
+      document.querySelector('.message').textContent =
+        'enter a number between 1 to 20';
+    }
+  } else {
+    document.querySelector('.score').textContent = 0;
+    document.querySelector('.message').textContent =
+      'You lose! Press Again to play again';
+    document.querySelector('.check').disabled = true;
+    checkDisabled = true;
+  }
 });
